7eb407d8d0cb85d4b905e78874d54f58eb93e6ca
muslimbeibytuly <muslimbeibytuly@gmail.com>
2024-06-06 15:45:55 +0500

    Refactor some code

--- src/clients/s3_client.py
+++ src/clients/s3_client.py
@@ -1,43 +1,6 @@
 from boto3 import client
-from pydantic import BaseModel, Field
 
-
-class HTTPHeaders(BaseModel):
-    accept_ranges: str = Field(
-        alias='accept-ranges',
-    )
-    server: str
-    strict_transport_security: str = Field(
-        alias='strict-transport-security',
-    )
-    vary: str
-    x_amz_id_2: str = Field(
-        alias='x-amz-id-2',
-    )
-    x_amz_request_id: str = Field(
-        alias='x-amz-request-id',
-    )
-    x_content_type_options: str = Field(
-        alias='x-content-type-options',
-    )
-    x_xss_protection: str = Field(
-        alias='x-xss-protection',
-    )
-    date: str
-
-
-class ResponseMetadata(BaseModel):
-    request_id: str = Field(alias='RequestId')
-    host_id: str = Field(alias='HostId')
-    http_status_code: int = Field(alias='HTTPStatusCode')
-    http_headers: HTTPHeaders = Field(alias='HTTPHeaders')
-    retry_attempts: int = Field(alias='RetryAttempts')
-
-
-class DeleteResponse(BaseModel):
-    response_meta_data: ResponseMetadata = Field(
-        alias='ResponseMetadata',
-    )
+from .models import DeleteResponse
 
 
 class S3Client:
